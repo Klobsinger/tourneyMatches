5ab6cf08f6116af68fd7bff05ad6e1475d346757
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-06 19:01:34 -0400

    Refactor PlayerList.js moved import logic and calling of helper functions to App.js

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,12 +1,7 @@
 import Player from "./Player";
-import matchData from "../data/matchData";
-import playerData from "../data/playerData";
-import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
 
 function PlayerList (props)  {
-  const onePlayer = playerData["Disguised Lizard"];
-  const playerDataArray = preparePlayerData(playerData);
-    const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const onePlayer = props.playerData[0];
   return (
     <section className="PlayerList">
     <h1>Current participating players</h1>
